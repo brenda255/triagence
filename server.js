55ad0197cb438335e39e6a2ca3d96d0e4c261d6c
brenda <115198624+brenda255@users.noreply.github.com>
2025-06-15 13:04:05 -0600

    updated server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.use(express.json());
 
 const HF_API_KEY = process.env.HUGGINGFACE_API_KEY;
 
+function detectUrgency(text) {
+    const lowerText = text.toLowerCase();
+
+    if (/(asap|urgent|immediately|right away|critical)/.test(lowerText)){
+        return "High";
+    } else if (/(soon|issue|problem|as soon as possible)/.test(lowerText)){
+        return "Medium";
+    } else if (/(no rush|not urgent|whenever you can|take your time)/.test(lowerText)){
+        return "Low";
+    } else {
+        return "Unknown";
+    }
+}
+
 app.post("/analyze", async(req,res) => {
     const {text, task} = req.body;
 
@@ -34,7 +48,12 @@ app.post("/analyze", async(req,res) => {
         },
     });
 
-    res.json(response.data);
+    const urgency = detectUrgency(text);
+
+    res.json({
+        result: response.data,
+        urgency: urgency,
+  });
   } catch (error) {
     console.error(error.response?.data || error.message);
     res.status(500).json({ error: "API request failed."});
@@ -45,3 +64,5 @@ const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
+
+
